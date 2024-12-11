b47055c70c35f254f43bcaf3d73427f3c59458d4
cdawson1223 <cd3417@columbia.edu>
2024-12-11 14:34:43 -0500

    all the little fixes

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app import app
 from scipy.optimize import curve_fit
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import os 
 # from sklearn.linear_model import LinearRegression
 # from sklearn.model_selection import train_test_split
 # from sklearn.metrics import mean_squared_error
@@ -12,20 +13,57 @@ import numpy as np
 @app.route('/')
 @app.route('/index')
 def index():
-    actual = 72
+    #actual = 72
     measured = 66
     humidity = 36
     ambient_light = 15000
     pressure = 30.43
     data_cols = handle_data('app/WeatherAppData.csv')
+    names_array = ["Brooklyn Apartment Window", "Brooklyn Apartment Radiator", "Brooklyn Apartment Hallway", "Brooklyn Apartment Living Room",
+                   "Manhattan Apartment Freezer", "Manhattan Apartment Heater", "Manhattan Apartment Bedroom"]
+    info_array = read_test_data()
+    print("thru test data")
     temp1, intercept = linear_regression(data_cols[0], data_cols[1], data_cols[2], data_cols[3], data_cols[4])
     other,a,b,c = temp1
-    actual = other*measured + a*humidity + b*ambient_light + c*pressure + intercept
-    return render_template('index.html', title='Home', actual = actual, measured = measured, humidity = humidity, ambient_light = ambient_light, pressure = pressure)
+    outputed_array = []
+    for index,item in enumerate(info_array):
+        location = names_array[index]
+        measured, humidity, pressure, ambient_light = item
+        measured = measured*(9/5) + 32 
+        pressure *= 0.0295301
+        actual = other*measured + a*humidity + b*ambient_light + c*pressure + intercept
+        outputed_array.append([location, round(measured,2), round(humidity,2), round(pressure,2), 
+                                round(ambient_light,2), round(actual,2)])
+    #print("before calc")
+    #actual = other*measured + a*humidity + b*ambient_light + c*pressure + intercept
+    #print("after calc")
+    #return render_template('index.html', title='Home', actual = actual, measured = measured, humidity = humidity, ambient_light = ambient_light, pressure = pressure)
+    return render_template('index.html', title='Home', output = outputed_array)
     #return "Hello, World!"
 
-def read_test_data(file):
-    return
+@app.route('/empty')
+def empty():
+    return render_template('empty.html')
+
+def read_test_data():
+    files = os.listdir('app/temp_readings')
+    print()
+    apt_metrics = []
+    print("the files are ", files)
+    for file_name in files: 
+        if os.path.splitext(file_name)[-1].lower() == '.csv':
+            file = 'app/temp_readings/' + file_name
+            print("file is: ", file)
+            df = pd.read_csv(file)
+            #print("test dataframe: ", file_name)
+            #print(df)
+            column_averages = df.mean(axis=0).to_list()[1:]
+            print ("col avgs: ", column_averages)
+            print("type of col is: ", type(column_averages))
+            apt_metrics.append(column_averages) #added in order of temp, humidity, pressure, and light levels 
+            print("round successful")
+    print("thru test data")
+    return apt_metrics
 
 
 #read in csv, return numpy arrays for measured_data, humidity_data, light_data, pressure_data, actual_data
